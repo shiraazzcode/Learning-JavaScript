1c548e863f8cbc6c313a51630c370bd039829a82
Shiraz Mehdi <144130813+shiraazzcode@users.noreply.github.com>
2024-06-19 09:34:58 +0000

    variables and datatypes

---
+++ 01jsbasics/datatypes.js
@@ -0,0 +1,27 @@
+//Data types
+//two types 
+//1) Primitive (2)Non primitive
+//Primitive - String, Number, BigInt, Boolean, Null, Undefined, and Symbol.
+//Non primitive -  reference or derived data types - eg objects, arrays
+
+let name = "Sheraz"; //String - data inside quotes
+let number = 123; //Numbers
+let isAlive = true ; // boolean - true/false
+let nothing = null; // null - empty - standalone value
+let msg; //undefined - not assigned yet
+
+
+//symbol - unique
+
+//Non primitive
+//Objects
+//Arrays
+
+
+//null is onject
+
+//typeOf operator - shows the type of data
+let str = "hello";
+console.log("data type :" + " " + typeof(str));
+
+
---
+++ 01jsbasics/variables.js
@@ -0,0 +1,11 @@
+//What are variables
+//variables are containers to store data in memory.
+//let and const are keywords to declear variables
+let myname = "Sheraz"; // can be updated but not changed by same name
+const name = "Mehdi"; //cant be chenged or updated
+var fulname = "Sheraz Mehdi"; //can be updated and changed. Not reccomended to use var.
+lastname = "Mehdi"; //can work but not a good practice.
+
+// console.log(myname);
+console.table([myname, name, fulname, lastname]);
+

507857897b7c3834d9af56fceec9370c36f86eb5
Shiraz Mehdi <144130813+shiraazzcode@users.noreply.github.com>
2024-06-21 08:48:07 +0000

    Strings

---
+++ 01jsbasics/strings.js
@@ -0,0 +1,7 @@
+//Strings 
+//srings are sequence of charaters enclosed in single or double quotes. 
+//eg - "strings", 'hello world' etc
+let str1 = "hello world";
+console.log(str1);
+console.log(typeof str1);
+
